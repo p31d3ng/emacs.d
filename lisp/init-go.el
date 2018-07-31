faa37526f52abbf7a27fc791bd985e4efe9530d1
Pei Deng <pdeng@vmware.com>
2018-07-31 14:48:20 -0700

    built-in golang support by flycheck?

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -1,38 +1,28 @@
 (require-package 'go-mode)
 (require-package 'company-go)
 
+;; add flyche-gometaliner to flycheck
 (require-package 'flycheck-gometalinter)
 (flycheck-gometalinter-setup)
-;;(setq flycheck-gometalinter-fast t)  ;; only run fast linters
-;;(setq flycheck-gometalinter-test t)  ;; use in tests files
+(setq flycheck-gometalinter-fast t)  ;; only run fast linters
+(setq flycheck-gometalinter-test t)  ;; use in tests files
 (add-hook 'go-mode-hook 'flycheck-mode)
 
+;; auto formatting before save
 (add-hook 'before-save-hook 'gofmt-before-save)
 
-(if (eq system-type 'windows-nt) ;; windows $GOPATH set to C:/gopath
-    (progn
-      (add-to-list 'load-path "~/gopath/src/github.com/dougm/goflymake")(require 'go-flymake)
-      (add-to-list 'load-path "~/gopath/src/github.com/dougm/goflymake")(require 'go-flycheck)
-      )
-  (progn
-    (add-to-list 'load-path "~/go/src/github.com/dougm/goflymake")(require 'go-flymake)
-    (add-to-list 'load-path "~/go/src/github.com/dougm/goflymake")(require 'go-flycheck)
-    )
-  )
-
+;; go code autocomplete
 (add-hook 'go-mode-hook 'company-mode)
 (add-hook 'go-mode-hook (lambda ()
                           (set (make-local-variable 'company-backends) '(company-go))
                           (company-mode)))
 
-
+;; go code keybindings
 (add-hook 'go-mode-hook '(lambda ()
                            (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
-
 (add-hook 'go-mode-hook '(lambda ()
-        (local-set-key (kbd "C-c C-g") 'go-goto-imports)))
-
+                           (local-set-key (kbd "C-c C-g") 'go-goto-imports)))
 (add-hook 'go-mode-hook '(lambda ()
-        (local-set-key (kbd "C-c C-k") 'godoc)))
+                           (local-set-key (kbd "C-c C-k") 'godoc)))
 
 (provide 'init-go)
